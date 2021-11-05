abd65d9994dfc72af3c348041eb1ad95ea13d1a5
Ronningen <step-lap@mail.ru>
2021-11-05 11:29:40 +0300

    interface fix

--- solar_main.py
+++ solar_main.py
@@ -118,32 +118,33 @@ def main():
     root = tkinter.Tk()
     # космическое пространство отображается на холсте типа Canvas
     space = tkinter.Canvas(root, width=window_width, height=window_height, bg="black")
-    space.pack(side=tkinter.TOP)
     # нижняя панель с кнопками
     frame = tkinter.Frame(root)
-    frame.pack(side=tkinter.BOTTOM)
 
     start_button = tkinter.Button(frame, text="Start", command=start_execution, width=6)
-    start_button.pack(side=tkinter.LEFT)
 
     time_step = tkinter.DoubleVar()
     time_step.set(1)
     time_step_entry = tkinter.Entry(frame, textvariable=time_step)
-    time_step_entry.pack(side=tkinter.LEFT)
 
     time_speed = tkinter.DoubleVar()
     scale = tkinter.Scale(frame, variable=time_speed, orient=tkinter.HORIZONTAL)
-    scale.pack(side=tkinter.LEFT)
 
     load_file_button = tkinter.Button(frame, text="Open file...", command=open_file_dialog)
-    load_file_button.pack(side=tkinter.LEFT)
     save_file_button = tkinter.Button(frame, text="Save to file...", command=save_file_dialog)
-    save_file_button.pack(side=tkinter.LEFT)
 
     displayed_time = tkinter.StringVar()
     displayed_time.set(str(physical_time) + " seconds gone")
     time_label = tkinter.Label(frame, textvariable=displayed_time, width=30)
+
+    frame.pack(side=tkinter.BOTTOM)
+    start_button.pack(side=tkinter.LEFT)
+    time_step_entry.pack(side=tkinter.LEFT)
+    scale.pack(side=tkinter.LEFT)
+    load_file_button.pack(side=tkinter.LEFT)
+    save_file_button.pack(side=tkinter.LEFT)
     time_label.pack(side=tkinter.RIGHT)
+    space.pack(side=tkinter.TOP)
 
     root.mainloop()
     print('Modelling finished!')
