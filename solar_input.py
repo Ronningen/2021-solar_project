d68830417825dda978d88633f5e12c8138eb2499
SamodelkinDA <samodelkin.da@phystech.edu>
2021-11-05 10:19:26 +0300

    Сделано чтение из файла

--- solar_input.py
+++ solar_input.py
@@ -20,10 +20,14 @@ def read_space_objects_data_from_file(input_filename):
             if len(line.strip()) == 0 or line[0] == '#':
                 continue  # пустые строки и строки-комментарии пропускаем
             object_type = line.split()[0].lower()
-            if object_type == "star":  # FIXME: do the same for planet
+            if object_type == "Star":  # FIXED: do the same for planet 
                 star = Star()
                 parse_star_parameters(line, star)
                 objects.append(star)
+            elif object_type == "Planet": 
+                planet = Planet()
+                parse_planet_parameters(line, planet)
+                objects.append(planet)
             else:
                 print("Unknown space object")
 
@@ -44,8 +48,14 @@ def parse_star_parameters(line, star):
     **line** — строка с описание звезды.
     **star** — объект звезды.
     """
-
-    pass  # FIXME: not done yet
+    massiv = list(line.strip().split())
+    star.R = int(massiv[1])
+    star.color = massiv[2]
+    star.m = int(massiv[3])
+    star.x = int(massiv[4])
+    star.y = int(massiv[5])
+    star.Vx = int(massiv[6])
+    star.Vy = int(massiv[7])
 
 def parse_planet_parameters(line, planet):
     """Считывает данные о планете из строки.
@@ -62,7 +72,14 @@ def parse_planet_parameters(line, planet):
     **line** — строка с описание планеты.
     **planet** — объект планеты.
     """
-    pass  # FIXME: not done yet...
+    massiv = list(line.strip().split())
+    planet.R = int(massiv[1])
+    planet.color = massiv[2]
+    planet.m = int(massiv[3])
+    planet.x = int(massiv[4])
+    planet.y = int(massiv[5])
+    planet.Vx = int(massiv[6])
+    planet.Vy = int(massiv[7])
 
 
 def write_space_objects_data_to_file(output_filename, space_objects):
