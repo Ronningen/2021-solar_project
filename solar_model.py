c71659c8c6b2fdd8d5593e68337604364eb805f2
SamodelkinDA <samodelkin.da@phystech.edu>
2021-11-05 09:53:55 +0300

    Поправлена функция calculate_force

--- solar_model.py
+++ solar_model.py
@@ -17,10 +17,13 @@ def calculate_force(body, space_objects):
     body.Fx = body.Fy = 0
     for obj in space_objects:
         if body == obj:
-            continue  # тело не действует гравитационной силой на само себя!
+            continue  # тело не действует гравитационной силой на само себя!\
+        # расстояние между телами
         r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        body.Fx += 1  # FIXME: нужно вывести формулу...
-        body.Fy += 2  # FIXME: нужно вывести формулу...
+        # вычисление общей комбинации GMm/r^3
+        gamma =  gravitational_constant * body.mass * obj.mass / r ** 3
+        body.Fx += -(body.x - obj.x) * gamma
+        body.Fy += -(body.y - obj.y) * gamma
 
 
 def move_space_object(body, dt):
