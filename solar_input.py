d432ea7a592646882087fc87af96531c6aaa8924
Ronningen <step-lap@mail.ru>
2021-11-05 16:14:31 +0300

    prestat

--- solar_input.py
+++ solar_input.py
@@ -2,6 +2,7 @@
 # license: GPLv3
 # Пробный коммит
 
+from os import lseek
 from solar_objects import Star, Planet
 
 
@@ -20,11 +21,11 @@ def read_space_objects_data_from_file(input_filename):
             if len(line.strip()) == 0 or line[0] == '#':
                 continue  # пустые строки и строки-комментарии пропускаем
             object_type = line.split()[0].lower()
-            if object_type == "star":  # FIXED: do the same for planet 
+            if object_type == "star":  # FIXED: do the same for planet
                 star = Star()
                 parse_star_parameters(line, star)
                 objects.append(star)
-            elif object_type == "planet": 
+            elif object_type == "planet":
                 planet = Planet()
                 parse_planet_parameters(line, planet)
                 objects.append(planet)
@@ -56,6 +57,7 @@ def parse_star_parameters(line, star):
     star.y = float(massiv[5])
     star.Vx = float(massiv[6])
     star.Vy = float(massiv[7])
+
 
 def parse_planet_parameters(line, planet):
     """Считывает данные о планете из строки.
@@ -95,13 +97,30 @@ def write_space_objects_data_to_file(output_filename, space_objects):
     """
     with open(output_filename, 'w') as out_file:
         for obj in space_objects:
-            out_file.write(obj.type + " " + str(obj.R) + " " + 
-                obj.color + " " + str(obj.m) + " " + str(obj.x) + " " + 
-                str(obj.y) + " " + str(obj.Vx) + " " + str(obj.Vy) + "\n")
+            out_file.write(obj.type + " " + str(obj.R) + " " +
+                           obj.color + " " + str(obj.m) + " " + str(obj.x) + " " +
+                           str(obj.y) + " " + str(obj.Vx) + " " + str(obj.Vy) + "\n")
             out_file.write("\n")
-            # FIXME: should store real values
 
-# FIXME: хорошо бы ещё сделать функцию, сохранающую статистику в заданный файл...
+
+def append_space_objects_stat_to_file(stat_filename, space_objects, t):
+    """Добавляет новую cтатистику космических объектов в файл.
+    Строки должны иметь следующий формат:
+    Star <радиус в пикселах> <цвет> <масса>:
+        <t>: <x> <y> <Vx> <Vy>
+        ...
+    Planet <радиус в пикселах> <цвет> <масса>:
+        <t>: <x> <y> <Vx> <Vy>
+        ...
+
+    Параметры:
+
+    **stat_filename** — имя входного файла
+    **space_objects** — список объектов планет и звёзд
+    **t** - время сохранения статистики
+    """
+    pass #FIXME
+
 
 if __name__ == "__main__":
     print("This module is not for direct call!")
